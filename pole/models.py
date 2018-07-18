4d5e62f481ebf2cadeba4be7bbf41dfdb31fe3ec
Mike Kutzma <michaelkutzma@gmail.com>
2018-07-17 21:00:47 -0400

    Start adding change models

--- pole/models.py
+++ pole/models.py
@@ -10,3 +10,25 @@ class User(models.Model):
 
 	def __str__(self):
 		return self.user_name
+
+
+class Change(models.Model):
+	user = models.ForeignKey(User,on_delete=models.CASCADE)
+	from_rank = models.IntegerField()
+	to_rank = models.IntegerField()
+	creation_date = models.DateTimeField('date created')
+
+	def __str__(self):
+		return "{} of {} from {} to {}.".format(self.get_changeword('noun').capitalize(),self.user.user_name,
+			self.from_rank,self.to_rank)
+
+	def get_changeword(self,tense):
+		move = self.to_rank-self.from_rank
+		ind = int(((move/abs(move))+1)/2)
+		tensedict = {
+			'past':['promoted','demoted'],
+			'future':['promote','demote'],
+			'noun':['promotion','demotion'],
+		}
+		return tensedict[tense][ind]
+
